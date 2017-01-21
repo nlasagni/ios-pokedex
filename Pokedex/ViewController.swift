5cbe0d4da04eed163cf5532471ad643ab3f83639
Nicola Lasagni <nicola.lasagni@gmail.com>
2017-01-21 16:04:16 +0100

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Nicola Lasagni on 21/01/2017.
+//  Copyright © 2017 Nicola Lasagni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
